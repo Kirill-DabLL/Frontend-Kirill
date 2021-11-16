c95531af330e46d033b5c63db91aa6ed66349eaa
Kirill <kg0545713@gmail.com>
2021-11-17 00:42:47 +0300

    Desire

--- app/js/main.js
+++ app/js/main.js
@@ -7,5 +7,38 @@ $(function () {
         $('.rightside-menu').addClass('rightside-menu--close');
     });
 
-    $('.top__slider').slick({});
+
+    $('.header__btn-menu').on('click', function () {
+        $('.menu').toggleClass('menu--open');
+    });
+
+    if($(window).width() < 651){
+        $('.works-path__item--measuring').appendTo($('.works-path__item-box'));
+    }
+    
+    $('.top__slider').slick({
+        dots: true,
+        arrows: false,
+        fade: true,
+        autoplay: true
+    });
+
+    $('.contact-slider').slick({
+        slidesToShow: 10,
+        slidesToScroll: 10,
+        dots: true,
+        arrows: false,
+    });
+
+    $('.article-slider__box').slick({
+        prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/arrow-slide-left.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrowright"><img src="images/arrow-slide-right.svg" alt=""></button>'
+    });
+
+    var mixer = mixitup('.gallery__inner', {
+        load: {
+            filter: '.living'
+        }
+    });
+    
 })
